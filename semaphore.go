7a7f3ff1e4942efd2ee524c1cae95b3a0918b87c
Abiola Ibrahim <abiola89@gmail.com>
2017-01-07 12:06:06 +0100

    Improve documentation

--- semaphore.go
+++ semaphore.go
@@ -1,18 +1,21 @@
 package semaphore
 
 import (
+	"errors"
 	"sync"
 	"time"
 )
 
+// Semaphore is an implementation of semaphore.
 type Semaphore struct {
 	permits int
 	avail   int
-	channel chan int
-	aMutex  *sync.Mutex
+	channel chan struct{}
+	aMutex  *sync.RWMutex
 	rMutex  *sync.Mutex
 }
 
+// New creates a new Semaphore with specified number of permits.
 func New(permits int) *Semaphore {
 	if permits < 1 {
 		panic("Invalid number of permits. Less than 1")
@@ -20,36 +23,40 @@ func New(permits int) *Semaphore {
 	return &Semaphore{
 		permits,
 		permits,
-		make(chan int, permits),
-		&sync.Mutex{},
+		make(chan struct{}, permits),
+		&sync.RWMutex{},
 		&sync.Mutex{},
 	}
 }
 
-//Acquire one permit, if its not available the goroutine will block till its available
+// Acquire acquires one permit. If it is not available, the goroutine will block until it is available.
 func (s *Semaphore) Acquire() {
 	s.aMutex.Lock()
-	s.channel <- 1
+	defer s.aMutex.Unlock()
+
+	s.channel <- struct{}{}
 	s.avail--
-	s.aMutex.Unlock()
 }
 
-//Similar to Acquire() but for many permits
-func (s *Semaphore) AcquireMany(n int) {
+// AcquireMany is similar to Acquire() but for many permits.
+// An error is returned if n is greater number of permits in the semaphore.
+func (s *Semaphore) AcquireMany(n int) error {
 	if n > s.permits {
-		panic("To many requested permits")
+		return errors.New("Too many requested permits")
 	}
 	s.aMutex.Lock()
+	defer s.aMutex.Unlock()
+
 	s.avail -= n
 	for ; n > 0; n-- {
-		s.channel <- 1
+		s.channel <- struct{}{}
 	}
 	s.avail += n
-	s.aMutex.Unlock()
+	return nil
 }
 
-//Similar to AcquireMany() but cancels if duration elapse before getting the permits.
-//Returns true if successful and false if timeout occurs.
+// AcquireWithin is similar to AcquireMany() but cancels if duration elapses before getting the permits.
+// Returns true if successful and false if timeout occurs.
 func (s *Semaphore) AcquireWithin(n int, d time.Duration) bool {
 	timeout := make(chan bool, 1)
 	cancel := make(chan bool, 1)
@@ -72,15 +79,16 @@ func (s *Semaphore) AcquireWithin(n int, d time.Duration) bool {
 	return true
 }
 
-//Release one permit
+// Release releases one permit.
 func (s *Semaphore) Release() {
 	s.rMutex.Lock()
+	defer s.rMutex.Unlock()
+
 	<-s.channel
 	s.avail++
-	s.rMutex.Unlock()
 }
 
-//Release many permits
+// ReleaseMany releases n permits.
 func (s *Semaphore) ReleaseMany(n int) {
 	if n > s.permits {
 		panic("Too many requested releases")
@@ -90,15 +98,18 @@ func (s *Semaphore) ReleaseMany(n int) {
 	}
 }
 
-//Number of available unacquired permits
+// AvailablePermits gives number of available unacquired permits.
 func (s *Semaphore) AvailablePermits() int {
+	s.aMutex.RLock()
+	defer s.aMutex.RUnlock()
+
 	if s.avail < 0 {
 		return 0
 	}
 	return s.avail
 }
 
-//Acquire all available permits and return the number of permits acquired
+// DrainPermits acquires all available permits and return the number of permits acquired.
 func (s *Semaphore) DrainPermits() int {
 	n := s.AvailablePermits()
 	if n > 0 {
